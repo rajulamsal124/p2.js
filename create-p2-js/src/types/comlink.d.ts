5437f2f7363da67acd8f25f6af9f55fe0ba37a1d
Raju Lamsal <rajulamsal@Rajus-MacBook-Air.local>
2024-12-04 18:27:22 +1000

    Reorganize repository: Move files to root and remove create-p2-js directory

--- create-p2-js/src/types/comlink.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'comlink' {
-  export function wrap<T>(endpoint: any): T;
-  export function expose(obj: any): void;
-  export function transfer(obj: any, transfers: ArrayBuffer[]): void;
-  export function proxy<T>(obj: T): T;
-}
